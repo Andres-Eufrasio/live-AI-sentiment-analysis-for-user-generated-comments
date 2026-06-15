3e043e996924ad263756bd51b0432a906307c449
Andres Eufrasio Tinajero <serdna102@gmail.com>
2026-06-15 17:27:55 +0100

    Changed Model file to predict and convert to a json format, changed it into a class strucutre called HFSentimentAnalysis

--- Model.py
+++ Model.py
@@ -10,31 +10,115 @@ continous loop for interaction
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 
-Sentiment_model = "unitary/toxic-bert"
+from fastapi import FastAPI
 
-#Download toxic bert
-tokenizer = AutoTokenizer.from_pretrained(Sentiment_model)
-model= AutoModelForSequenceClassification.from_pretrained(Sentiment_model)
+"""
+A sentiement analysis predictor that loads a huging face model
+Precondition:
+Comment text
 
-#switch to inference model for predicting without training
-model.eval()
+Postcondition:
+json with labels and probability
+"""
+class HFSentimentAnalysis():
 
-test_text = "fuck off mom"
+    def __init__(self):
 
-#convert to pythorch tensors 
-input = tokenizer(test_text, return_tensors="pt", truncation=True, padding=True)
+        self.name = "unitary/toxic-bert"
+        try:   
+            self.tokenizer = AutoTokenizer.from_pretrained(self.name)
+            self.model = AutoModelForSequenceClassification.from_pretrained(self.name)
+        except Exception as ex:
+            print(f"failed to load model {ex}")
+        self.labels = self.model.config.id2label
+        self.model.eval()
 
-#don't track gradient since I'm not doing training
-with torch.no_grad():
-    output = model(**input)
+    def get_name(self):
+        return self.name
+    
+    def shutdown_model(self):
+        del self.model
+        torch.cuda.empty_cache()
+        
+    def convert_to_json(self, prediction):
+        # [0] included because when tesnor is converted into a list it becomes a 2dArray with the contents in the first position.
+        label_prediction = prediction.tolist()[0]
+        json_prediction = {}
+        print(label_prediction)
+        
+        for label,label_prediction in zip(self.labels.values(), label_prediction):
+            json_prediction.update({label: label_prediction})
+            
+        print(json_prediction)
+        return json_prediction
+
+    def predict(self,text : str, format=True):
+        #convert to pythorch tensors 
+        input = self.tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+
+        with torch.no_grad():
+            output = self.model(**input)
+
+        logits = output.logits
+        prediction = torch.softmax(logits, dim=1)
+        if format == True:
+            json_prediction = self.convert_to_json(prediction)
+            return json_prediction
+        else:
+            return prediction
+            
+
+class PredictionSystem():
+    app = FastAPI()
+        
+
+class LocalSentimentAnalysis():
+    pass
 
 
-logits = output.logits
-#reduce dementionality to get final results 
-probs = torch.softmax(logits, dim=1)
+class comment():
+    pass
 
 
-#toxic, severe_toxic, obscene, threat, insult, and identity_hate
-print("Logits:", logits)
-print("Probabilities:", probs)
+if __name__ == "__main__":
+    start = HFSentimentAnalysis()
+    start.predict("text")
+
+
+
+
+    """
+    
+    print("tet")
+    Sentiment_model = "unitary/toxic-bert"
+
+    #Download toxic bert
+    tokenizer = AutoTokenizer.from_pretrained(Sentiment_model)
+    model= AutoModelForSequenceClassification.from_pretrained(Sentiment_model)
+
+    #switch to inference model for predicting without training
+    model.eval()
+
+    test_text = "fuck off mom"
+
+    #convert to pythorch tensors 
+    input = tokenizer(test_text, return_tensors="pt", truncation=True, padding=True)
+
+    #don't track gradient since I'm not doing training
+    with torch.no_grad():
+        output = model(**input)
+
+
+    logits = output.logits
+    #reduce dementionality to get final results 
+    probs = torch.softmax(logits, dim=1)
+
+
+    #toxic, severe_toxic, obscene, threat, insult, and identity_hate
+    labels = model.config.id2label
+    print(labels) 
+    print("Logits:", logits)
+    print("Probabilities:", probs)
+    print(float(probs[0][0]))
+    """
 
